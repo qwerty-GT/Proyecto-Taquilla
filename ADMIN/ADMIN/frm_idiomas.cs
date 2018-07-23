539f8a1edf88a22fee270c396d36afcede9b6dc7
qwerty-GT <qwerty.enterprises@gmail.com>
2018-07-23 07:42:15 -0600

    Mantenimientos
    Utilizar esta APP

---
+++ ADMIN/ADMIN/frm_idiomas.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace ADMIN
+{
+    public partial class frm_idiomas : Form
+    {
+        frm_main menu;
+        public frm_idiomas(frm_main anterior)
+        {
+            InitializeComponent();
+            menu = anterior;
+            actualizar();
+        }
+
+        private void Btn_formatoAceptar_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT ID_IDIOMA FROM IDIOMA WHERE ID_IDIOMA = '{0}'", textBox2.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (!reader.Read())
+                    {
+                        conexion.Close();
+                        conexion = TaquillaDB.getDB();
+                        sql = string.Format("INSERT INTO IDIOMA VALUES('{0}');", textBox2.Text);
+                        cmd = new OdbcCommand(sql, conexion);
+                        cmd.ExecuteNonQuery();
+                        textBox2.Text = "";
+                        textBox2.Focus();
+                        MessageBox.Show("GUARDADO!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else
+                    {
+                        MessageBox.Show("IDIOMA EXISTENTE!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+                MessageBox.Show("COMPLETE TODOS LOS CAMPOS!!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu.Show();
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT * FROM IDIOMA WHERE ID_IDIOMA = '{0}'", textBox4.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        dataGridView2.Rows.Clear();
+                        dataGridView2.Rows.Add(reader.GetString(0));
+                        textBox4.Text = "";
+                        textBox3.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("IDIOMA NO ENCONTRADO!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    textBox4.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA UN IDIOMA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox4.Focus();
+            }
+        }
+
+        private void actualizar()
+        {
+            OdbcConnection conexion = TaquillaDB.getDB();
+            try
+            {
+                string sql = string.Format("SELECT * FROM IDIOMA;");
+                OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                OdbcDataReader reader = cmd.ExecuteReader();
+                dataGridView2.Rows.Clear();
+                while (reader.Read())
+                {
+                    dataGridView2.Rows.Add(reader.GetString(0));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conexion.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("DELETE FROM IDIOMA WHERE ID_IDIOMA = '{0}'", textBox4.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    int ban = cmd.ExecuteNonQuery();
+                    if (ban == 1)
+                    {
+                        MessageBox.Show("IDIOMA ELIMINADO!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        actualizar();
+                    }
+                    else
+                        MessageBox.Show("IDIOMA NO ENCONTRADO!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox4.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA UN IDIOMA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox4.Focus();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false; textBox1.Text = "";
+            textBox3.Enabled = true;
+            button10.Enabled = true;
+            button2.Enabled = true;
+            button7.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT * FROM IDIOMA WHERE ID_IDIOMA = '{0}'", textBox3.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        textBox1.Enabled = true;
+                        textBox3.Enabled = false;
+                        button10.Enabled = false;
+                        button2.Enabled = false;
+                        button7.Enabled = true;
+                        button6.Enabled = true;
+                        textBox1.Text = reader.GetString(0);
+                    }
+                    else
+                    {
+                        MessageBox.Show("IDIOMA NO ENCONTRADO!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA UN IDIOMA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox3.Focus();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT ID_IDIOMA FROM IDIOMA WHERE ID_IDIOMA = '{0}'", textBox1.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (!reader.Read() || (textBox3.Text == textBox1.Text))
+                    {
+                        conexion.Close();
+                        conexion = TaquillaDB.getDB();
+                        sql = string.Format("UPDATE IDIOMA SET ID_IDIOMA = '{0}' WHERE ID_IDIOMA = '{1}';", textBox1.Text, textBox3.Text);
+                        cmd = new OdbcCommand(sql, conexion);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("ACTUALIZADO!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        textBox1.Enabled = false; textBox1.Text = "";
+                        textBox3.Enabled = true; textBox3.Text = "";
+                        textBox4.Text = "";
+                        button10.Enabled = true;
+                        button2.Enabled = true;
+                        button7.Enabled = false;
+                        button6.Enabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("IDIOMA EXISTENTE!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+                MessageBox.Show("COMPLETE TODOS LOS CAMPOS!!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox4.Text = this.dataGridView2.CurrentRow.Cells[0].Value.ToString();
+            if (textBox3.Enabled)
+                textBox3.Text = this.dataGridView2.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void frm_idiomas_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            menu.Show();
+        }
+    }
+}
---
+++ ADMIN/ADMIN/frm_salas.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace ADMIN
+{
+    public partial class frm_salas : Form
+    {
+        frm_main menu;
+        public frm_salas(frm_main anterior)
+        {
+            InitializeComponent();
+            menu = anterior;
+            actualizar();
+            cine();
+            formato();
+        }
+
+        private void cine()
+        {
+            OdbcConnection conexion = TaquillaDB.getDB();
+            try
+            {
+                string sql = string.Format("SELECT ID_CINE FROM CINE;");
+                OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                OdbcDataReader reader = cmd.ExecuteReader();
+                comboBox1.Items.Clear();
+                comboBox4.Items.Clear();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader.GetString(0));
+                    comboBox4.Items.Add(reader.GetString(0));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                this.Close();
+            }
+            conexion.Close();
+        }
+
+        private void formato()
+        {
+            OdbcConnection conexion = TaquillaDB.getDB();
+            try
+            {
+                string sql = string.Format("SELECT ID_FORMATO FROM FORMATO;");
+                OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                OdbcDataReader reader = cmd.ExecuteReader();
+                comboBox2.Items.Clear();
+                comboBox3.Items.Clear();
+                while (reader.Read())
+                {
+                    comboBox2.Items.Add(reader.GetString(0));
+                    comboBox3.Items.Add(reader.GetString(0));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                this.Close();
+            }
+            conexion.Close();
+        }
+
+        private void frm_salas_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            menu.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            menu.Show();
+            this.Close();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_gestionFormatos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actualizar()
+        {
+            OdbcConnection conexion = TaquillaDB.getDB();
+            try
+            {
+                string sql = string.Format("SELECT * FROM SALA;");
+                OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                OdbcDataReader reader = cmd.ExecuteReader();
+                dataGridView2.Rows.Clear();
+                while (reader.Read())
+                {
+                    dataGridView2.Rows.Add(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conexion.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void tabControl1_Click(object sender, EventArgs e)
+        {
+            actualizar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT * FROM SALA WHERE ID_SALA = '{0}'", textBox4.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        dataGridView2.Rows.Clear();
+                        dataGridView2.Rows.Add(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5));
+                        textBox4.Text = "";
+                        textBox3.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("SALA NO ENCONTRADA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    textBox4.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA ID SALA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox4.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("DELETE FROM SALA WHERE ID_SALA = '{0}'", textBox4.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    int ban = cmd.ExecuteNonQuery();
+                    if (ban == 1)
+                    {
+                        MessageBox.Show("SALA ELIMINADA!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        actualizar();
+                    }
+                    else
+                        MessageBox.Show("SALA NO ENCONTRADA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox4.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA UN ID SALA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox4.Focus();
+            }
+        }
+
+        private void Btn_formatoAceptar_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && textBox5.Text != "" && comboBox1.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("INSERT INTO SALA VALUES(NULL, {0}, {1}, {2}, '{3}', {4});", textBox2.Text, textBox5.Text, comboBox1.Text, comboBox3.Text, textBox7.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    cmd.ExecuteNonQuery();
+                    textBox2.Text = "";
+                    textBox5.Text = "";
+                    textBox7.Text = "";
+                    comboBox1.SelectedIndex = -1;
+                    comboBox3.SelectedIndex = -1;
+                    textBox2.Focus();
+                    MessageBox.Show("GUARDADO!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+                MessageBox.Show("COMPLETE TODOS LOS CAMPOS!!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("SELECT * FROM SALA WHERE ID_SALA = '{0}'", textBox3.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    OdbcDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        textBox1.Enabled = true;
+                        textBox6.Enabled = true;
+                        textBox8.Enabled = true;
+                        comboBox2.Enabled = true;
+                        comboBox4.Enabled = true;
+                        textBox3.Enabled = false;
+                        button10.Enabled = false;
+                        button2.Enabled = false;
+                        button7.Enabled = true;
+                        button6.Enabled = true;
+                        textBox6.Text = reader.GetString(1);
+                        textBox1.Text = reader.GetString(2);
+                        comboBox4.SelectedIndex = comboBox4.FindString(reader.GetString(3));
+                        comboBox2.SelectedIndex = comboBox2.FindString(reader.GetString(4));
+                        textBox8.Text = reader.GetString(5); textBox8.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("SALA NO ENCONTRADA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+            {
+                MessageBox.Show("ESCRIBA UN SALA!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox3.Focus();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false; textBox1.Text = "";
+            textBox6.Enabled = false; textBox6.Text = "";
+            textBox8.Enabled = false; textBox8.Text = "";
+            textBox3.Enabled = true;
+            button10.Enabled = true;
+            button2.Enabled = true;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            comboBox2.Enabled = false; comboBox2.SelectedIndex = -1;
+            comboBox4.Enabled = false; comboBox4.SelectedIndex = -1;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox6.Text != "" && textBox6.Text != "" && comboBox4.Text != "" && comboBox2.Text != "")
+            {
+                OdbcConnection conexion = TaquillaDB.getDB();
+                try
+                {
+                    string sql = string.Format("UPDATE SALA SET FILAS_SALA = {0}, COLUMNAS_SALA = {1}, ID_CINE = {2}, ID_FORMATO = '{3}', NUMERO_SALA = '{4}' WHERE ID_SALA = {5}", textBox6.Text, textBox1.Text, comboBox4.Text, comboBox2.Text, textBox8.Text, textBox3.Text);
+                    OdbcCommand cmd = new OdbcCommand(sql, conexion);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("ACTUALIZADO!", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    textBox1.Enabled = false; textBox1.Text = "";
+                    textBox6.Enabled = false; textBox6.Text = "";
+                    textBox3.Enabled = true; textBox3.Text = "";
+                    textBox8.Enabled = false; textBox8.Text = "";
+                    textBox4.Text = "";
+                    comboBox2.SelectedIndex = -1; comboBox2.Enabled = false;
+                    comboBox4.SelectedIndex = -1; comboBox4.Enabled = false;
+                    button10.Enabled = true;
+                    button2.Enabled = true;
+                    button7.Enabled = false;
+                    button6.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                conexion.Close();
+            }
+            else
+                MessageBox.Show("COMPLETE TODOS LOS CAMPOS!!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox4.Text = this.dataGridView2.CurrentRow.Cells[0].Value.ToString();
+            if (textBox3.Enabled)
+                textBox3.Text = this.dataGridView2.CurrentRow.Cells[0].Value.ToString();
+        }
+    }
+}
